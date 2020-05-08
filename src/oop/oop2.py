4609b65aecd1db8b843fa5c19b7259ea0335984f
Phong Yang <phongyang88@hotmail.com>
2020-05-08 12:50:06 -0500

    removed extra super call in __init__ of motorcycle

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -23,7 +23,6 @@ class GroundVehicle():
 class Motorcycle(GroundVehicle):
     def __init__(self, num_wheels=2):
         super().__init__(num_wheels)
-        super().drive()
     
     def drive(self):
         return "BRAAAP!!"
